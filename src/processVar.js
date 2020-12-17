420a6a989f595488f327f704cecfe436c6c4716a
Michael J. Dole <webmaster@mjduniverse.com>
2020-12-17 12:56:27 -0600

    Adding JSDoc documentation
    Adding PhSim.addGlobalMagicalWord
    function
    
    Adding
    PhSim.prototype.addLocalMagicalWord function

--- src/processVar.js
+++ src/processVar.js
@@ -19,24 +19,104 @@ PhSim.MagicWords = {
 		return "4";
 	},
 
+	/**
+	 * The `__game__score` magical word returns the game score if the game widget is enabled.
+	 * @function
+	 * @returns {Number} - Game score.
+	 */
+
 	__game__score: function() {
 		return this.lclGame && this.lclGame.score;
 	},
+
+	/**
+	 * The `__game__life` magical word returns the live count of the player if the 
+	 * game widget is enabled.
+	 * 
+	 * @function
+	 * @returns {Number} - Life count
+	 */
 
 	__game__life: function() {
 		return this.lclGame && this.lclGame.life; 
 	},
 
+	/**
+	 * The `__game__goal` magical word returns the goal of the game if the game widget
+	 * is enabled.
+	 * 
+	 * @function
+	 * @returns {Number}
+	 */
+
 	__game__goal: function() {
 		return this.lclGame && this.lclGame.goal;
 	},
+
+	/**
+	 * The `__game__int_life` magical word returns the intial life count of the game,
+	 * given the game widget is enabled.
+	 * 
+	 * @function
+	 * @returns {Number}
+	 */
 
 	__game__int_life: function() {
 		return this.lclGame && this.lclGame.intLife;
 	},
 
+	/**
+	 * The `__game__int_score` magical word returns the inital game score given the 
+	 * game widget is enabled.
+	 * 
+	 * @function
+	 * @returns {Number}
+	 */
+
 	__game__int_score: function() {
 		return this.lclGame && this.lclGame.intScore;
+	}
+
+}
+
+/**
+ * 
+ * Adds a global magical word function.
+ * 
+ * @function
+ * @param {String} name - Name of magical word
+ * @param {Function} call 
+ */
+
+PhSim.addGlobalMagicalWord = function(name,call) {
+	
+	if(PhSim.MagicWords[name]) {
+		throw "Magical word " + name + " already exists."
+	}
+
+	else {
+		PhSim.MagicWords[name] = call;
+	}
+
+}
+
+/**
+ * 
+ * Adds a local magical word function.
+ * 
+ * @function
+ * @param {String} name - Name of magical word
+ * @param {Function} call 
+ */
+
+PhSim.prototype.addLocalMagicalWord = function(name,call) {
+
+	if(this.magicWords[name]) {
+		throw "Magical word " + name + " already exists."
+	}
+
+	else {
+		this.magicWords[name] = call;
 	}
 
 }
