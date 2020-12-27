7163bb555dd154babef4c113a6baac8fd2a776dd
Michael J. Dole <webmaster@mjduniverse.com>
2020-12-27 13:50:54 -0600

    Deleting build.sh script
    Making "npx webpack" the build script
    
    Moving ink-docstrap to devDepdencies
    
    Adding jsdoc to devDependiences

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,46 @@
 const path = require('path');
 
-module.exports = {
-    entry: [
-        "./src/phSim.js",
-        "./widgets-js-gen/widgetTypes.js"
-    ],
-    mode: "production",
-    node: false,
-    optimization: {
-        minimize: false,
-        //namedChunks: false,
-        //concatenateModules: false
+module.exports = [
+
+    // Non-minified
+
+    {
+        entry: "./src/phSim.js",
+        mode: "production",
+        node: false,
+        optimization: {
+            minimize: false,
+        },
+    
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: "phsim.js",
+            globalObject: 'this',
+        },
+    
+        externals: {
+            "matter-js": "commonjs matter-js"
+        }
     },
 
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: "phsim.js",
-        globalObject: 'this',
-    },
+    // Minified Version
 
-    externals: {
-        "matter-js": "commonjs matter-js"
+    {
+        entry: "./src/phSim.js",
+        mode: "production",
+        node: false,
+        optimization: {
+            minimize: true,
+        },
+    
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: "phsim.min.js",
+            globalObject: 'this',
+        },
+    
+        externals: {
+            "matter-js": "commonjs matter-js"
+        }
     }
-};
+];
