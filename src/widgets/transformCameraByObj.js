4007f839f968776472d68f56c31ef24c172fd572
Michael J. Dole <webmaster@mjduniverse.com>
2021-01-28 09:57:41 -0600

    Fixing drift glitch.

--- src/widgets/transformCameraByObj.js
+++ src/widgets/transformCameraByObj.js
@@ -14,7 +14,7 @@ PhSim.Widgets.transformCameraByObj = function(dyn_object) {
     
     var self = this;
 
-    this.on("afterupdate",function(){
+    this.on("beforeupdate",function(){
         var dx = dyn_object.matter.position.x - dyn_object.matter.positionPrev.x;
         var dy = dyn_object.matter.position.y - dyn_object.matter.positionPrev.y;
         self.camera.translate(-dx,-dy);
