615219c463229979ddfe95225c5969a4e5180933
Michael J. Dole <webmaster@mjduniverse.com>
2021-02-17 14:34:00 -0600

    Making functions return an instance of PhSim.Widget.

--- src/widgets/setRenderProperties.js
+++ src/widgets/setRenderProperties.js
@@ -1,4 +1,5 @@
 const PhSim = require("../index");
+const Widget = require("../widget")
 
 /**
  * 
@@ -9,16 +10,25 @@ const PhSim = require("../index");
  * @param {PhSim.DynObject} dyn_object - Dynamic Object that will have it's color changed.
  * @param {WFunctionOptions} widget - Widget Options
  * @param {String} widget.color - The new color of the object.
- *
+ * @returns {PhSim.Widget}
  */
 
 PhSim.Widgets.setColor = function(dyn_object,widget) {
+
+    var self = this;
+
+    var w = new Widget(dynObject,widget);
 
     var f = function() {
         PhSim.DynObject.setColor(dyn_object,widget.color);
     }
 
-    this.createWFunction(dyn_object,f,widget);
+    w.wFunction = self.createWFunction(dyn_object,f,widget);
+
+    dyn_object.test1 = w;
+
+    return w;
+
 }
 
 /**
@@ -29,9 +39,13 @@ PhSim.Widgets.setColor = function(dyn_object,widget) {
  * @param {PhSim.DynObject} dyn_object 
  * @param {WFunctionOptions} widget - Widget properties.
  * @param {String} widget.color - The new color of the object border
+ * @returns {PhSim.Widget}
+ * 
  */
     
 PhSim.Widgets.setBorderColor = function(dyn_object,widget) {
+
+    var w = new Widget(dyn_object,widget);
 
     var closure = function() {
 
@@ -46,7 +60,9 @@ PhSim.Widgets.setBorderColor = function(dyn_object,widget) {
 
     }
 
-    this.createWFunction(dyn_object,closure(),widget);
+    w.wFunction = this.createWFunction(dyn_object,closure(),widget);
+
+    return w;
 }
 
 /**
@@ -58,14 +74,19 @@ PhSim.Widgets.setBorderColor = function(dyn_object,widget) {
  * @param {PhSim.DynObject} dyn_object - The object to be affected.
  * @param {WFunctionOptions} widget - Widget options
  * @param {Number} widget.width - New line width
+ * @returns {PhSim.Widget}
  * 
  */
         
 PhSim.Widgets.setLineWidth = function(dyn_object,widget) {
 
+    var w = new Widget(dyn_object,widget);
+
     var f = function(){
         PhSim.DynObject.setLineWidth(dyn_object,widget.width);
     }
 
-    this.createWFunction(dyn_object,f,widget);
+    w.wFunction = this.createWFunction(dyn_object,f,widget);
+
+    return w;
 }
