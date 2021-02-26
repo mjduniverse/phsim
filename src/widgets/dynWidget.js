fac905087db21dcbcc9828bd7e020e7e5ffe19a1
Michael J. Dole <webmaster@mjduniverse.com>
2021-02-25 21:07:18 -0600

    Moving connectDynObject function.

--- src/widgets/dynWidget.js
+++ src/widgets/dynWidget.js
@@ -15,31 +15,6 @@ else {
 const DynObject = require("../dynObject");
 const PhSim = require("..");
 
-// Object Connection
-
-PhSim.prototype.connectDynObjects = function(parent,child) {
-
-	Matter.Body.setStatic(child,true);
-
-	var f = function() {
-
-		var v = {
-			"x": parent.matter.position.x - parent.matter.positionPrev.x,
-			"y": parent.matter.position.y - parent.matter.positionPrev.y,
-		}
-
-		PhSim.Motion.translate(child,v);
-
-		PhSim.Motion.rotate(child,parent.matter.angle - parent.matter.anglePrev,parent.matter.position);
-
-	}
-
-	this.on("afterupdate",f)
-
-	return f;
-
-}
-
 /**
  * 
  * Run function on all objects.
@@ -60,22 +35,6 @@ PhSim.prototype.forAllObjects = function(call) {
 	}
 }
 
-/**
- * Add object to over layer.
- * 
- * @function
- * @param {PhSim.DynObject} dynObject 
- */
-
-PhSim.prototype.addToOverlayer = function(o) {
-	
-	if(o instanceof DynObject) {
-		Matter.World.add(this.matterJSWorld, o.matter);
-	}
-
-	this.objUniverse.push(o);
-
-}
 
 /**
  * Check if the object is a dynamic object.
@@ -86,69 +45,6 @@ PhSim.prototype.addToOverlayer = function(o) {
 
 PhSim.prototype.isNonDyn = function(o) {
 	return o.noDyn;
-}
-
-/**
- * 
- * Add Object to PhSim simulation
- * 
- * @function
- * @param {PhSimObject} o 
- * @param {Object} options
- * @param {Number} options.layer 
- * @returns {PhSim.DynObject} - The added dynObject. 
- */
-
-PhSim.prototype.addObject = function(o,options = {}) {
-
-	if(typeof options.layer === "number") {
-		this.dynTree[options.layer].push(o);
-
-		if(o instanceof DynObject) {
-			o.layerBranch = this.dynTree[options.layer];
-		}
-
-	}
-
-	this.objUniverse.push(o);
-
-	if(o instanceof DynObject) {
-
-		o.phSim = this;
-
-		Matter.World.add(this.matterJSWorld,o.matter);
-
-		if(o.static.widgets) {
-			this.extractWidgets(o);
-		}
-
-	}
-
-	return o;
-}
-
-/**
- * Remove dynamic object
- * 
- * @function
- * @param {PhSim.DynObject}  dynObject - Dynamic Object
- * @returns {PhSim.DynObject} - The removed Dynamic Object
- */
-
-PhSim.prototype.removeDynObj = function(dynObject) {
-
-	Matter.Composite.remove(this.matterJSWorld,dynObject.matter);
-
-	this.objUniverse.splice(this.objUniverse.indexOf(dynObject),1);
-
-	if(dynObject.layerBranch) {
-		var i = dynObject.layerBranch.indexOf(dynObject);
-		dynObject.layerBranch.splice(i,1);
-		dynObject.layerBranch = undefined;
-	}
-
-	return dynObject;
-
 }
 
 /**
